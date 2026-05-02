8c396aef7f5752b1796d5a9400aaa6676775c798
Mark Bigelow <mjbigelow@gmail.com>
2026-05-02 05:55:20 +0000

    Pre-autobuild: source sync 2026-05-02 05:55

--- chore_checker/lib/main.dart
+++ chore_checker/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-const String SUPABASE_URL = 'https://knxylewjxdhscaokxywzap.supabase.co';
+const String SUPABASE_URL = 'https://knyyeyjxdscaokyywzap.supabase.co';
 const String SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtueXlleWp4ZHNjYW9reXl3emFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc2NDc5MTAsImV4cCI6MjA5MzIyMzkxMH0.IlzXCj9Jl1WQyBg4Mm7oM5b9GFNMFzvPBi6a90QShQw';
 
 void main() async {
